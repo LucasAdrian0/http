a32b2b7e8643569f1fce6b1425fb4e2d13a734d9
Lucas Adriano <lukasadriano94@hotmail.com>
2026-01-05 11:16:33 -0300

    criando e inicalizado o banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,35 @@ import 'package:trilhaapp/model/dados_cadastrais_model.dart';
 import 'package:trilhaapp/model/tarefa_hive_model.dart';
 import 'package:trilhaapp/my_app.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as path;
+
+Map<int, String> scripts = {
+  1: ''' CREAT TABLE taredas(
+        id INT PRIMARY KEY AUTOINCREMENT,
+        descricao TEXT,
+        concluido INTERGER
+        );''',
+};
+
+//metodo para inicializar o banco de dados
+Future iniciarBancoDeDados() async {
+  var db = await openDatabase(
+    path.join(await getDatabasesPath(), 'database.db'),
+    version: scripts.length,
+    onCreate: (Database db, int version) async {
+      for (var i = 1; i < scripts.length; i++) {
+        await db.execute(scripts[i]!);
+      }
+    },
+    onUpgrade: (Database db, int oldVersion, int newVersion) async {
+      for (var i = oldVersion; i < scripts.length; i++) {
+        await db.execute(scripts[i]!);
+      }
+    },
+  );
+  return db;
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +41,7 @@ void main() async {
   Hive.init(documentsDirectory.path);
   Hive.registerAdapter(DadosCadastraisModelAdapter());
   Hive.registerAdapter(TarefaHiveModelAdapter());
+  await iniciarBancoDeDados();
   runApp(const MyApp());
 }
 
